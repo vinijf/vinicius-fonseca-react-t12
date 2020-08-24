901f993a5a9f5691768353656913b0df7920f914
Vinícius Fonseca <vinijf@live.com>
2020-08-24 15:04:33 -0300

    15:04 segunda-feira, 24 de agosto de 2020

--- src/components/Artigos.js
+++ src/components/Artigos.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import AppBar from "@material-ui/core/AppBar";
 import Button from "@material-ui/core/Button";
 import Card from "@material-ui/core/Card";
 import CardActions from "@material-ui/core/CardActions";
@@ -7,28 +6,13 @@ import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Grid from "@material-ui/core/Grid";
-import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
-import Link from "@material-ui/core/Link";
 import ArtigoApi from "../api/ArtigoApi";
 import { toast } from "react-toastify";
 import Head from "./Head";
 import Footer from "./Footer";
-
-function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {"Copyright © "}
-      <Link color="inherit" href="https://material-ui.com/">
-        Your Website
-      </Link>{" "}
-      {new Date().getFullYear()}
-      {"."}
-    </Typography>
-  );
-}
 
 const useStyles = makeStyles((theme) => ({
   icon: {
@@ -117,7 +101,7 @@ export default function Artigos() {
                         </CardContent>
                         <CardActions>
                           <Button
-                            onClick={(event) =>
+                            onClick={() =>
                               (window.location.href = `artigo/show/${card.id}`)
                             }
                             size="small"
--- src/components/Head.js
+++ src/components/Head.js
@@ -14,7 +14,7 @@ function Copyright() {
     <Typography variant="body2" color="textSecondary" align="center">
       {"Copyright © "}
       <Link color="inherit" href="https://material-ui.com/">
-        Your Website
+        Fatec Rio Preto
       </Link>{" "}
       {new Date().getFullYear()}
       {"."}
